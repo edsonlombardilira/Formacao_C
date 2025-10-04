61f68b474c617052b81cb5cd21f8cd725f4bce08
Edson_Dev <edson.lombardi.lira@gmail.com>
2025-10-04 16:05:56 -0300

    Feat: Adição de seção para calculo de dois numeros

--- dia_01/sinal.c
+++ dia_01/sinal.c
@@ -5,7 +5,8 @@ int main() {
     setlocale(LC_ALL, "Portuguese_Brazil.1252"); // define a localização para português do Brasil
 
 
-    char sinal; // declara uma variável do tipo caractere para armazenar o sinal da operação
+    char sinal;  // declara uma variável do tipo caractere para armazenar o sinal da operação
+    float num1, num2, resultado; // declara variáveis do tipo float para armazenar os números e o resultado
 
     do { // inicia um loop que continuará até que um sinal válido seja inserido
 
@@ -14,6 +15,38 @@ int main() {
 
     if (sinal == '+' || sinal == '-' || sinal == '*' || sinal == '/') { // verifica se o sinal é um dos válidos
         printf("Operação valida!\n");
+        printf("Digite o primeiro numero:\n");
+        scanf("%f" , &num1); // lê o primeiro número inserido pelo usuário
+        printf("Digite o segundo numero:\n");
+        scanf("%f" , &num2); // lê o segundo número inserido pelo usuário
+
+        switch (sinal)
+        {
+        case '+':
+            resultado = num1 + num2;
+            printf("O resultado da soma é: %.f\n", resultado);
+            break;
+        
+        case '-':
+            resultado = num1 - num2;
+            printf("O resultado da subtração é: %.f\n", resultado);
+            break;
+        case '*':
+            resultado = num1 * num2;
+            printf("O resultado da multiplicação é: %.f\n", resultado);
+            break;
+        case '/':
+            if (num2 != 0) { // verifica se o segundo número não é zero
+                resultado = num1 / num2;
+                printf("O resultado da divisão é: %.f\n", resultado);
+            } else {
+                printf("Erro: Divisão por zero não é permitida.\n");
+            }            
+        
+        default:
+            break;
+        }
+
     } else
     {
        printf("Operação invalida!\n");  
@@ -24,3 +57,4 @@ int main() {
 }
 
 // fim do programa
+
